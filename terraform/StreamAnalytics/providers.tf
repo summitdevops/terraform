9a864976c814bf4cca409fd31fe6cd44c200976d
summitdevops <59125824+summitdevops@users.noreply.github.com>
2020-06-14 11:47:14 -0500

    Update providers.tf

--- terraform/StreamAnalytics/providers.tf
+++ terraform/StreamAnalytics/providers.tf
@@ -1,7 +1,8 @@
 provider "azurerm" {
-	version = "~> 1.20"
+	version = "~> 2.0"
 	subscription_id = var.subscription-id
 	client_id       = var.client-id
 	client_secret   = var.client-secret
 	tenant_id       = var.tenant-id
+	features{}
 }
